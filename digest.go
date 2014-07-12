1a877340d2ee3026e06af0e686d6384dc1f81d55
Gwilym Evans <gwilym.evans@bigcommerce.com>
2014-07-12 21:08:04 +1000

    working proof of concept

---
+++ digest.go
@@ -0,0 +1,92 @@
+package main
+
+// credit: whoever did http://play.golang.org/p/ABoHSHoTmu
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Authorization struct {
+	Username, Password, Realm, NONCE, QOP, Opaque, Algorithm string
+}
+
+func GetAuthorization(username, password string, resp *http.Response) *Authorization {
+	header := resp.Header.Get("www-authenticate")
+	parts := strings.SplitN(header, " ", 2)
+	parts = strings.Split(parts[1], ",")
+	opts := make(map[string]string)
+
+	for _, part := range parts {
+		vals := strings.SplitN(strings.Trim(part, " "), "=", 2)
+		key := vals[0]
+		val := strings.Trim(vals[1], "\",")
+		opts[key] = val
+	}
+
+	auth := Authorization{
+		username, password,
+		opts["realm"], opts["nonce"], opts["qop"], opts["opaque"], opts["algorithm"],
+	}
+	return &auth
+}
+
+func SetDigestAuth(r *http.Request, username, password string, resp *http.Response, nc int) {
+	auth := GetAuthorization(username, password, resp)
+	ApplyDigestAuth(r, auth, nc)
+}
+
+func ApplyDigestAuth(r *http.Request, auth *Authorization, nc int) {
+	auth_str := GetAuthString(auth, r.URL, r.Method, nc)
+	r.Header.Add("Authorization", auth_str)
+}
+
+func GetAuthString(auth *Authorization, url *url.URL, method string, nc int) string {
+	a1 := auth.Username + ":" + auth.Realm + ":" + auth.Password
+	h := md5.New()
+	io.WriteString(h, a1)
+	ha1 := hex.EncodeToString(h.Sum(nil))
+
+	h = md5.New()
+	a2 := method + ":" + url.Path
+	io.WriteString(h, a2)
+	ha2 := hex.EncodeToString(h.Sum(nil))
+
+	nc_str := fmt.Sprintf("%08x", nc)
+	hnc := "MTM3MDgw"
+
+	respdig := fmt.Sprintf("%s:%s:%s:%s:%s:%s", ha1, auth.NONCE, nc_str, hnc, auth.QOP, ha2)
+	h = md5.New()
+	io.WriteString(h, respdig)
+	respdig = hex.EncodeToString(h.Sum(nil))
+
+	base := "username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", response=\"%s\""
+	base = fmt.Sprintf(base, auth.Username, auth.Realm, auth.NONCE, url.Path, respdig)
+	if auth.Opaque != "" {
+		base += fmt.Sprintf(", opaque=\"%s\"", auth.Opaque)
+	}
+	if auth.QOP != "" {
+		base += fmt.Sprintf(", qop=\"%s\", nc=%s, cnonce=\"%s\"", auth.QOP, nc_str, hnc)
+	}
+	if auth.Algorithm != "" {
+		base += fmt.Sprintf(", algorithm=\"%s\"", auth.Algorithm)
+	}
+
+	// r.Header.Add("Authorization", "Digest " +base)
+	return "Digest " + base
+}
+
+// func main() {
+//     auth := Authorization{
+//         "Joe", "Schmoe", "RETS Server",
+//         "0decbedc0f7828a0f8a0f0ea4a2107e3", "auth",
+//         "a0531450f6a92cda6b30ae8e4de1cd2b", "",
+//     }
+//     myURL := url.URL{"http", "", nil, "www.dis.com:6103", "/rets/login", "", ""}
+//     fmt.Println(GetAuthString(&auth, &myURL, "POST", 3))
+// }
--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
+
+const TaskDirectionUp = 1
+const TaskDirectionDown = 2
 
 var directory_path string
 var prefix_len int
 var webdav_url string
+var digest_auth *Authorization
+var nonce_counter uint64
+var tasks chan *task
+
+type task struct {
+	path      string
+	direction int
+}
 
 func usage() {
 	fmt.Fprintln(os.Stderr, "WebDAV bulk upload and download tool")
@@ -35,30 +52,146 @@ func visitDirectory(path string, f os.FileInfo, err error) error {
 	return nil
 }
 
+func processTask(directory_path string, task *task) {
+	file, err := os.Open(directory_path + "/" + task.path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := webdav_url + "/" + task.path
+
+	if stat.IsDir() {
+		// MKCOL
+		// TODO need to ensure collections are created first
+		// TODO adjust bc server side so this isn't required
+		req, err := http.NewRequest("MKCOL", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if digest_auth != nil {
+			nc := int(atomic.AddUint64(&nonce_counter, 1))
+			ApplyDigestAuth(req, digest_auth, nc)
+		}
+
+		log.Println("Creating", task.path)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		log.Println(task.path, resp.Status)
+	} else {
+		// PUT
+
+		pr, pw := io.Pipe()
+		bufin := bufio.NewReader(file)
+
+		go func() {
+			bufin.WriteTo(pw)
+			pw.Close()
+		}()
+
+		req, err := http.NewRequest("PUT", url, pr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if digest_auth != nil {
+			nc := int(atomic.AddUint64(&nonce_counter, 1))
+			ApplyDigestAuth(req, digest_auth, nc)
+		}
+
+		log.Println("Sending", task.path)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if !(resp.StatusCode >= 200 && resp.StatusCode < 300) {
+			log.Fatal(task.path, " failed with status ", resp.Status)
+		}
+	}
+}
+
 func main() {
 	// var local = flag.String("local", "", "Local directory path")
 	// var url = flag.String("host", "", "WebDAV URL")
 	// var username = flag.String("username", "", "WebDAV Username")
 	// var password = flag.String("password", "", "WebDAV Password")
-	//
 
 	threads := flag.Int("n", 5, "")
-	tasks := make(chan *string, 64)
+	tasks = make(chan *task, *threads+10)
 
 	flag.Usage = usage
 	flag.Parse()
 
 	directory_path = strings.TrimRight(flag.Arg(0), "/")
 	prefix_len = len(directory_path) + 1
-	webdav_url = flag.Arg(1)
+	webdav_url = strings.TrimRight(flag.Arg(1), "/")
+	task_direction := TaskDirectionUp
+
+	parsed_url, err := url.Parse(webdav_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !(parsed_url.Scheme == "http" || parsed_url.Scheme == "https") {
+		log.Fatal("Invalid url")
+	}
+
+	log.Println("Connecting to", parsed_url.Host, "...")
+
+	resp, err := http.Head(webdav_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 401 && resp.Header["Www-Authenticate"] != nil {
+		nonce_counter = 1
+		username := parsed_url.User.Username()
+		log.Println("Logging in as", username, "...")
+		password, _ := parsed_url.User.Password()
+		parsed_url.User = nil
+		digest_auth = GetAuthorization(username, password, resp)
+		digest_req, err := http.NewRequest("HEAD", parsed_url.String(), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ApplyDigestAuth(digest_req, digest_auth, int(nonce_counter))
+		digest_resp, err := http.DefaultClient.Do(digest_req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer digest_resp.Body.Close()
+
+		if digest_resp.StatusCode != 200 {
+			log.Fatal("Login failed with status ", digest_resp.Status)
+		}
+		webdav_url = parsed_url.String()
+	} else if resp.StatusCode != 200 {
+		log.Fatal("Connection failed with status ", resp.Status)
+	}
 
 	var wg sync.WaitGroup
 
 	for i := 0; i < *threads; i++ {
 		wg.Add(1)
 		go func() {
-			for foo := range tasks {
-				fmt.Println("Shall process", directory_path, *foo)
+			for task := range tasks {
+				processTask(directory_path, task)
 			}
 			wg.Done()
 		}()
@@ -66,12 +199,26 @@ func main() {
 
 	filepath.Walk(directory_path, func(path string, f os.FileInfo, err error) error {
 		if len(path) > prefix_len {
-			relative_path := path[prefix_len:]
-			if relative_path != "" {
-				tasks <- &relative_path
+			if f.IsDir() {
+				relative_path := path[prefix_len:]
+				tasks <- &task{path: relative_path, direction: task_direction}
 			}
 		}
 		return nil
 	})
+
+	// TODO the process may have to wait until collections exist otherwise if
+	//      the requests below could commence too early
+
+	filepath.Walk(directory_path, func(path string, f os.FileInfo, err error) error {
+		if len(path) > prefix_len {
+			if !f.IsDir() {
+				relative_path := path[prefix_len:]
+				tasks <- &task{path: relative_path, direction: task_direction}
+			}
+		}
+		return nil
+	})
+
 	close(tasks)
 }
